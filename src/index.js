47fbde94c9042c7dd7b8f1d72ad1660caebff702
ShockIgorek <shokin1996@yandex.ru>
2021-12-15 02:59:15 +0700

    start functional

--- src/index.js
+++ src/index.js
@@ -1,36 +1,22 @@
 import './index.css';
 
-const question = document.querySelector('.question')
-const hint = document.querySelector('.hint')
-const checkButton = document.querySelector('.check-button')
-//тут словарь
-const dictionaryEng = {
-    0: "Time", // под ключом "name" хранится значение "John"
-    1: "Year",
-    2: "People",
-    3: "Way",
-    4: "Day",
-    5: "Man",
-    6: "Thing",
-    7: "Woman",
-    8: "Life",
-    9: "Child",
-    10: "World",
-}
 
-const dictionaryRus = {
-    0: "Время", // под ключом "name" хранится значение "John"
-    1: "Год",
-    2: "Люди",
-    3: "Путь",
-    4: "День",
-    5: "Мужик",
-    6: "Вещь",
-    7: "Дама",
-    8: "Жизнь",
-    9: "Ребенок",
-    10: "Мир",
-}
+const hint = document.querySelector('.hint');
+const checkButton = document.querySelector('.check-button');
+const question = document.querySelector('.question');
+const answer = document.querySelector('.answer')
+
+let responseCount = 0;
+let trueCount = 0;
+let stockCount = 0.5;
+
+
+
+//тут словарь
+import {dictionaryEng} from './scripts/module/dictionaryEng.js'
+import {dictionaryRus} from './scripts/module/dictionaryRus.js'
+
+
 
 
 let randomizerHint = (text) => {
@@ -51,54 +37,54 @@ let resetOpacity = (selector) => {
     selector.style.opacity = 0;
 }
 
+let random = Math.floor(Math.random() * (Object.keys(dictionaryEng).length))
+let check = () => {
+    let ans = answer.value.toLowerCase();
+    let quest = dictionaryRus[random].toLowerCase();
+
+    if (ans === quest) {
+        trueCount++
+        update()
+    } else {
+        update()
+    }
+}
+
 
 let update = () => {
+    random = Math.floor(Math.random() * (Object.keys(dictionaryEng).length))
     resetOpacity(question);
     resetOpacity(hint);
-    let rand = Math.floor(Math.random() * (Object.keys(dictionaryEng).length));
+    // let rand = Math.floor(Math.random() * (Object.keys(dictionaryEng).length));
     setTimeout(() => {
         opacity(question);
         hint.style.opacity = 0.3;
-        question.textContent = dictionaryEng[rand];
-        hint.textContent = randomizerHint(dictionaryRus[rand]);
-    }, 400);
+        question.textContent = dictionaryEng[random];
+        hint.textContent = randomizerHint(dictionaryRus[random]);
 
+        responseCount = responseCount + 1;
+        answer.value = ""
+    }, 400);
+    stockCount = trueCount / responseCount;
+    console.log(stockCount)
+    speed()
 }
 
 
 window.addEventListener('load', update);
+window.addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        check()
+    }
+});
 checkButton.addEventListener('click', update);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //спижометр
-checkButton.addEventListener('click', function () {
-    let rand = Math.floor(Math.random() * 200 - 100);
+const speed = () => {
+    let rand = stockCount * 200 - 100;
     document.querySelector(".arrow").style.transform = `rotate(${rand}deg`;
-    num.innerHTML = parseInt((230 * (Number(rand) + 100)) / 180);
-});
-
-
-
-
-
-
-
-
+    num.innerHTML = Math.round(stockCount * 100) + "%"
+};
